f12afd506784db6fa8acaf359d0854a031e04025
Tarcizio B <tarcizio.barbosa@outlook.com>
2021-04-20 21:08:51 -0300

    create settings table and configs

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import express from 'express';
 
 import './database';
+import { routes } from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ Hello: 'World' });
-});
-
-app.post('/users', (request, response) => {
-  return response.json({ message: 'Usuário cadastrado com sucesso!' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log('Server is running on port 3333'));
