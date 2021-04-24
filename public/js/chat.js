b29cecdfcc710acb18031b14432239f9588af87f
Tarcizio B <tarcizio.barbosa@outlook.com>
2021-04-23 21:21:38 -0300

    create a web socket server

---
+++ public/js/chat.js
@@ -0,0 +1,27 @@
+document.querySelector('#start_chat').addEventListener('click', (event) => {
+  const socket = io();
+
+  const chatHelp = document.getElementById('chat_help');
+  chatHelp.style.display = 'none';
+
+  const chatInSupport = document.getElementById('chat_in_support');
+  chatInSupport.style.display = 'block';
+
+  const email = document.getElementById('email').value;
+  const text = document.getElementById('txt_help').value;
+
+  socket.on('connect', () => {
+    const params = {
+      email,
+      text,
+    };
+
+    socket.emit('client_first_acess', params, (call, err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(call);
+      }
+    });
+  });
+});
